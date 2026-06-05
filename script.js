1f5443bee7a7d827765ebe94c7d1eefa12ef20e3
NorikoOnishi <nrk024@yahoo.co.jp>
2026-06-05 15:45:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,56 @@
 function openWork(n){
-  document.getElementById("top").style.display="none";
-  document.getElementById("work"+n).style.display="block";
+  document.getElementById("top").style.display = "none";
+  document.getElementById("work"+n).style.display = "block";
 }
 
 function back(){
   document.querySelectorAll(".work-page").forEach(p=>{
-    p.style.display="none";
+    p.style.display = "none";
   });
-  document.getElementById("top").style.display="block";
+  document.getElementById("top").style.display = "block";
 }
 
 /* ===== ギャラリー自動生成 ===== */
 
-const works = [
-  {id:1,file:"001.jpg"},
-  {id:2,file:"002.jpg"},
-  {id:3,file:"003.JPG"},
-  {id:4,file:"004.JPG"},
-  {id:5,file:"005.JPG"},
-  {id:6,file:"006.JPG"},
-  {id:7,file:"007.JPG"},
-  {id:8,file:"008.JPG"},
-  {id:9,file:"009.jpg"},
-  {id:200,file:"200.JPG"}
-];
-
-const gallery=document.getElementById("gallery");
+const gallery = document.getElementById("gallery");
 
 works.forEach(w=>{
-  const img=document.createElement("img");
-  img.src=w.file;
-  img.onclick=()=>openWork(w.id);
+  const img = document.createElement("img");
+  img.src = w.file;
+  img.onclick = () => openWork(w.id);
   gallery.appendChild(img);
 });
 
 /* ===== カルーセル ===== */
 
-const images=["001.jpg","kobe-niki.JPG","002.jpg","garage.JPG","009.jpg"];
+const images = [
+  "001.jpg",
+  "kobe-niki.JPG",
+  "002.jpg",
+  "garage.JPG",
+  "009.jpg"
+];
 
-let i=0;
-const img=document.getElementById("carousel-image");
+let current = 0;
+const carouselImg = document.getElementById("carousel-image");
 
-function show(){
-  img.src=images[i];
+function showImage(){
+  carouselImg.src = images[current];
 }
 
-document.querySelector(".next").onclick=()=>{
-  i=(i+1)%images.length;
-  show();
+document.querySelector(".next").onclick = ()=>{
+  current = (current + 1) % images.length;
+  showImage();
 };
 
-document.querySelector(".prev").onclick=()=>{
-  i=(i-1+images.length)%images.length;
-  show();
+document.querySelector(".prev").onclick = ()=>{
+  current = (current - 1 + images.length) % images.length;
+  showImage();
 };
 
-show();
+showImage();
 
 setInterval(()=>{
-  i=(i+1)%images.length;
-  show();
+  current = (current + 1) % images.length;
+  showImage();
 },8000);
